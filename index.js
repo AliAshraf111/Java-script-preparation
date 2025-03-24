c5ed8757640193a75d80e76c65b4521189ed7207
Ali Ashraf <aliashrafkhokhar111@gmail.com>
2025-03-24 21:52:08 +0500

    add my js code

---
+++ index.js
@@ -0,0 +1,28 @@
+ var x="hmz";
+ var x="hamza"; we can also redeclare
+ x=12  we can also reasign value in var casse
+ {
+     var x=4;
+
+// } x is accessble outside of block
+// var is a global scope
+
+
+// Let is LOcal scopr varible 
+// Let just can reassign value but not dedecalre varible
+
+// function xyz(){
+//     var ab=10;
+//     if(true){
+//         let ab=20;
+//         console.log(ab)
+//     }
+//     console.log(ab)
+// }
+
+// xyz();
+
+// decaleration 
+// var a=10;
+// var a=20;
+// console.log(a)
